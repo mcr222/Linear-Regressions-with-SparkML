b17a1b2635fd747ab6ecaf559fafb24d41272353
mcr222 <markitus222@hotmail.com>
2017-11-15 21:42:52 +0100

    Reformated and added comments. Changed year and parameter grid.

--- src/main/scala/se/kth/spark/lab1/task4/Main.scala
+++ src/main/scala/se/kth/spark/lab1/task4/Main.scala
@@ -35,15 +35,15 @@ object Main {
       .setPattern(",")
       
      val arr2Vect = new Array2Vector()
-    .setInputCol("tokens")
-     .setOutputCol("tokens_vector")
+         .setInputCol("tokens")
+         .setOutputCol("tokens_vector")
      
      val lSlicer = new VectorSlicer().setInputCol("tokens_vector").setOutputCol("year")
     lSlicer.setIndices(Array(0))
 
      val v2d = new Vector2DoubleUDF((x: Vector) => x(0).toDouble).setInputCol("year").setOutputCol("label")
 
-     val min_year = 1921 //Change to the real one!!
+     val min_year = 1922
      val lShifter = new DoubleUDF((x:Double) => x-min_year).setInputCol("label").setOutputCol("label_shifted")
      
      
@@ -52,16 +52,17 @@ object Main {
     
      
      // Linear regression related transformations ------------------------
-    val myLR = new LinearRegression().setElasticNetParam(0.1).setRegParam(0.9).setMaxIter(50).setLabelCol("label_shifted").setFeaturesCol("features")
+    val myLR = new LinearRegression().setElasticNetParam(0.1).setRegParam(0.9).setMaxIter(50)
+        .setLabelCol("label_shifted").setFeaturesCol("features")
     val pipeline = new Pipeline().setStages(Array(regexTokenizer, arr2Vect, lSlicer, v2d, lShifter, fSlicer, myLR))
    
-
     //Hyperparameter related operations
     //Build the parameter grid
+    // We choose as base parameters (from task3) maxIter around 50 and regParam around 0.9
     val paramGrid = new ParamGridBuilder()
-    .addGrid( myLR.maxIter, Array(20,30,40,60,70,80))
-    .addGrid(myLR.regParam, Array(0.4, 0.6, 0.8, 0.92, 0.95, 1.0))
-    .build()
+      .addGrid(myLR.maxIter, Array(20, 30, 40, 50, 60, 70, 80))
+      .addGrid(myLR.regParam, Array(0.4, 0.6, 0.8, 0.9, 1.0, 1.2, 1.4))
+      .build()
     
     
     val evaluator = new RegressionEvaluator
@@ -71,25 +72,32 @@ object Main {
     val train = splits(0).cache()
     val test = splits(1).cache()
     
+    println("Cross validating all models")
     //Cross validation
     val cvModel: CrossValidator = new CrossValidator()
-    .setEstimator(pipeline)
-    .setEvaluator(evaluator)
-    .setEstimatorParamMaps(paramGrid)
-    .setNumFolds(8)
-
+        .setEstimator(pipeline)
+        .setEvaluator(evaluator)
+        .setEstimatorParamMaps(paramGrid)
+        //TODO: why 8 folds?
+        .setNumFolds(8)
+    
     val c = cvModel.fit(train)
+    println("Finished cross validating all models")
+    
     val lrModel = c.bestModel.asInstanceOf[PipelineModel].stages(6).asInstanceOf[LinearRegressionModel]
-   
+    println("Best model maximum iterations")
+    println(lrModel.getMaxIter)
+    println("Best model regularization parameter")
+    println(lrModel.getRegParam)
     //print rmse of our model
-   val trainingSummary = lrModel.summary
-   Predef println(
+    val trainingSummary = lrModel.summary
+    Predef println(
        "Root mean squared error:" + trainingSummary.rootMeanSquaredError +
        "\n Mean squared error " + trainingSummary.meanSquaredError + 
        "\n Mean absolute error " + trainingSummary.meanAbsoluteError)
    
     //do prediction - print first k
     val result = c.transform(test)
-    result.show(10)
+    result.drop("value", "tokens", "tokens_vector", "year").show(10)
   }
 }
