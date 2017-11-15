6d5980b57c652f8a9cbfa2699b59aab23283a331
Diego <o90G03e1258145>
2017-11-15 11:38:17 +0100

    - Progress task 6: VectorHelper.scala

--- src/main/scala/se/kth/spark/lab1/task6/VectorHelper.scala
+++ src/main/scala/se/kth/spark/lab1/task6/VectorHelper.scala
@@ -1,10 +1,14 @@
 package se.kth.spark.lab1.task6
 
 import org.apache.spark.ml.linalg.{Matrices, Vector, Vectors}
+import scala.collection.breakOut
 
 object VectorHelper {
   def dot(v1: Vector, v2: Vector): Double = {
-    ???
+   val v1_array = v1.toArray
+   val v2_array = v2.toArray
+
+   (v1_array zip v2_array).map{ Function.tupled(_ * _)}.sum
   }
 
   def dot(v: Vector, s: Double): Vector = {
@@ -12,10 +16,13 @@ object VectorHelper {
   }
 
   def sum(v1: Vector, v2: Vector): Vector = {
-    ???
+   
+    val t = Vectors.dense((v1.toArray, v2.toArray).zipped.map(_ + _))
+    t
   }
 
   def fill(size: Int, fillVal: Double): Vector = {
     ???
   }
+  
 }
